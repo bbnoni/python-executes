72276ecd11df5f4c9f6bd5b2bcca014fc1b2668a
benoniokaikoi <derhain@gmail.com>
2025-02-18 10:10:18 +0000

    addition of new screens new req.

---
+++ main.py
@@ -0,0 +1,64 @@
+
+
+# name = input("Enter your name: ")
+#
+# age = input("Enter your age: ")
+#
+# print(f"Hello, {name}!, You are {age} years old.")
+
+# x = 10
+# y = 5.5
+# z = "Python"
+#
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# print(float(100))
+# print(float(3.14))
+# print(int(5.67))
+
+# a = "5"
+# b = 3
+# print(a * b)
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+# print(num1 + num2)
+
+
+# age = int(input("Enter your age: "))
+#
+# if age < 18:
+#     print("You are a minor")
+# else:
+#     print("You are an adult")
+
+number = int(input("Enter a number: "))
+
+if number%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# password = input("Enter password: ")
+#
+# if password == "Python123":
+#     print("Access Granted!")
+# else:
+#     print("Access Denied!")
+
+# score = float(input("Enter your score: "))
+#
+# if score > 90 and score <= 100:
+#     print(f"Your score is {score}. \ A")
+# elif score >= 80 and score <= 89:
+#     print("B")
+# elif score >= 70 and score <= 79:
+#     print("C")
+# elif score >= 60 and score <= 69:
+#     print("D")
+# elif score < 60:
+#     print("F")
+# else:
+#     print("Enter score 100 and below")
